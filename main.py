1540c44272b0d08b71077a9f35d766b4b8494e3c
wu-jeffrey <j387wu@edu.uwaterloo.ca>
2019-01-14 19:59:35 -0500

    single button click event, icon changed

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import tkinter as tk
+from tkinter import ttk
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+def keypress(event):
+    """Simulate Keypress or Macro"""
+    keyboard.press('a')
+    print("bang")
+
 root = tk.Tk()
+root.iconbitmap(default="icon.ico")
+root.title("OneNote Helper")
+root.geometry("500x100")
 root.wm_attributes("-topmost", 1)
 
-w = tk.Label(root, text="DERP Tkinter!")
-w.pack()
+frame = tk.Frame(root)
+frame.pack()
+
+button = ttk.Button(frame, text='COPY')              
+button.pack()
+button.bind('<Button-1>', keypress)
 
 root.mainloop()
+
